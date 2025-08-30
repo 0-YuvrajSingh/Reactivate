9cc91c35b19fbd0c0062cb5f52545aa377a9a7de
Yuvraj Singh <uvi58493@gmail.com>
2025-08-30 09:51:24 +0530

    refactor: improve code formatting and readability in App component

--- 10-vite-toDo/src/App.jsx
+++ 10-vite-toDo/src/App.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
 import { ToDoProvider } from "./context";
+import { TodoForm, TodoItem } from "./components";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addToDo = (todo) => {
-    setTodos((prev) => [{id:Date.now(), ...todo}, ...prev]);
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
 
   const updateToDo = (id, todo) => {
-    setTodos((prev) => prev.map((prevToDo) => prevToDo.id === id ?
-    todo : prevToDo));
+    setTodos((prev) =>
+      prev.map((prevToDo) => (prevToDo.id === id ? todo : prevToDo))
+    );
   };
 
   const deleteToDo = (id) => {
@@ -18,13 +20,18 @@ function App() {
   };
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevToDo) => prevToDo.id === id ?
-     { ...prevToDo, completed: !prevToDo.completed } : prevToDo));
+    setTodos((prev) =>
+      prev.map((prevToDo) =>
+        prevToDo.id === id
+          ? { ...prevToDo, completed: !prevToDo.completed }
+          : prevToDo
+      )
+    );
   };
 
   useEffect(() => {
     const todos = JSON.parse(localStorage.getItem("todos"));
-    if(todos && todos.length) {
+    if (todos && todos.length) {
       setTodos(todos);
     }
   });
@@ -34,15 +41,26 @@ function App() {
   }, [todos]);
 
   return (
-    <ToDoProvider value = {{ todos, addToDo, updateToDo, deleteToDo, toggleComplete}}>
+    <ToDoProvider
+      value={{ todos, addToDo, updateToDo, deleteToDo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">{/* Todo form goes here */}
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}
+              className="w-full"
+              >
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
