1df5be429d3fe63bc494c2d26b10845d210da6c8
Yuvraj Singh <uvi58493@gmail.com>
2025-09-07 21:43:44 +0530

    feat: implement AuthService for Appwrite integration

---
+++ 12-vite-megaBlog/src/appwrite/auth.js
@@ -0,0 +1,25 @@
+import config from './config/config'
+import {Client, Account, ID} from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            await this.account.create(ID.unique(), email, password, name);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+export default authService
