a84a2e7c941bb93a6e42c4cafb4f0d1ea5aff912
Yuvraj Singh <uvi58493@gmail.com>
2025-09-28 16:07:14 +0530

    app related additions

--- 12-vite-megaBlog/src/App.jsx
+++ 12-vite-megaBlog/src/App.jsx
@@ -1,10 +1,40 @@
-import config from "./config/config";
+import { useState, useEffect } from "react";
+import {useDispatch} from "react-redux";
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice";
+import {Header, Footer} from "./components"
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(config.appwriteUrl);
-  return (
-    <div>A blog app</div>
-  )
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      dispatch(logout())
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div>
+      <Header />
+      <main>
+      TODO:  <Outlet />
+      </main>
+      <Footer />
+    </div>
+  ) : null ;
 }
 
 export default App
